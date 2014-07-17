dafb88c20e6bdf87c24737e4083f8e4df44a5baf
elliott828 <elliott828@users.noreply.github.com>
2014-07-17 15:05:38 +0800

    Update Modif.R

--- Modif.R
+++ Modif.R
@@ -1,4 +1,4 @@
-Modif <- function(pred, data, co.r = NaN, pc.r = NaN, sc1 = NaN, sc2 = NaN){
+Modif <- function(pred, data, co.r = NaN, pc.r = NaN, sc.1 = NaN, sc.2 = NaN){
   # transform specified variable with parameters
   # this function **return a dataset** with certain variable transformed
   # make sure the raw data is already loaded into global environment
@@ -15,11 +15,11 @@ Modif <- function(pred, data, co.r = NaN, pc.r = NaN, sc1 = NaN, sc2 = NaN){
   if(is.na(co.r)){
     # if no parameter for co rate, then no transformation happens
     message(paste("No transformation for variable '", pred, "'!", sep=""))
-  }else if(is.na(sc1)){
+  }else if(is.na(sc.1)){
     # if sc parameters is NA, then do co+pc transformation
     df[[pred]] <- pc(co(df[[pred]], co.r), pc.r)
   }else{
-    df[[pred]] <- sc(co(df[[pred]], co.r), sc1, sc2)
+    df[[pred]] <- sc(co(df[[pred]], co.r), sc.1, sc.2)
   }
   return(df)
 }
