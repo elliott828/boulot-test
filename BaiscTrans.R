e93ae51f634629db3dd8dc2e2101f2561c3cfa75
elliott828 <elliott828@users.noreply.github.com>
2014-09-01 11:14:06 +0800

    Update BaiscTrans.R

--- BaiscTrans.R
+++ BaiscTrans.R
@@ -330,10 +330,11 @@ ContM <- function(resp, data){
   }
   
   # return a list of both model result and updated data frame
-  return(list(fit, df))
+  return(list(fit, df, prmt))
   
 }
 
 # 8/7/2014: Creation - mdl.smry(), listing basic summary, MAPE and dwtest
 # 8/29/2014: Creation - ContM(), continue modeling based on the list of variables and their transformation parameters
-# 9/1/2014: Update - ContM(), bug fixed on reading predictor to be transformed
+# 9/1/2014: Update - ContM(), bug fixed on reading predictor to be transformed - all data will be coerced to char concerning variable names
+# 9/1/2014: Update - ContM(), full dataset of prmt is a part of new list returned at the end of this program
